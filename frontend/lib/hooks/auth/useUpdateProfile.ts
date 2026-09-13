9c24c0ac80050086a817d1ad610f4636cbe793b0
raheemnbrk <dicou940@gmail.com>
2026-09-13 10:40:18 +0100

    fix update profile.

--- frontend/lib/hooks/auth/useUpdateProfile.ts
+++ frontend/lib/hooks/auth/useUpdateProfile.ts
@@ -1,21 +1,16 @@
 import { updateProfileApi } from "@/lib/api/authApi";
-import { useAuthStore } from "@/lib/store/authStore";
 import { updateProfileInput } from "@/types/authTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 export const useUpdateProfile = () => {
   const queryClient = useQueryClient();
-  const setUser = useAuthStore((state) => state.setUser);
-
   const { mutate: updateProfile, isPending } = useMutation({
     mutationFn: (input: updateProfileInput & { file?: File }) =>
       updateProfileApi(input, input.file),
 
     onSuccess: (data) => {
       toast.success(data?.message ?? "Profile updated");
-
-      setUser(data.user);
 
       queryClient.invalidateQueries({
         queryKey: ["me"],
